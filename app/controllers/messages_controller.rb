8ecf340052bc7e8c12ffd82ca9754f722c7f4342
Youko Tada <counter0430culture@yahoo.co.jp>
2017-02-04 09:59:35 +0000

    Implementaion of the validation

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -6,13 +6,18 @@ class MessagesController < ApplicationController
   
   def create
     @message = Message.new(message_params)
-    @message.save
+    if @message.save
     redirect_to root_path , notice: 'メッセージを保存しました'
   end
+  
+else
+  @messages = Message.all
+  flash.now[:alert] = "メッセージの保存に失敗しました。"
+  render 'index'
+end
   
   private
   def message_params
     params.require(:message).permit(:name, :body)
   end
- 
 end
