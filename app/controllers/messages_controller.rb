003312e6f7d46b9ea3f85db44321d516b979a058
Youko Tada <counter0430culture@yahoo.co.jp>
2017-02-04 14:51:39 +0000

    Completed message-board

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,23 +1,46 @@
 class MessagesController < ApplicationController
+  before_action :set_message, only: [:edit, :update, :destroy]
+  
   def index
     @message = Message.new
+    # Messageを全て取得する。
     @messages = Message.all
   end
-  
+
   def create
     @message = Message.new(message_params)
     if @message.save
-    redirect_to root_path , notice: 'メッセージを保存しました'
+      redirect_to root_path, notice: 'メッセージを保存しました'
+    else
+      @messages = Message.all
+      flash.now[:alert] = 'メッセージの保存に失敗しました'
+      render 'index'
+    end
+  end
+
+  def edit
   end
   
-else
-  @messages = Message.all
-  flash.now[:alert] = "メッセージの保存に失敗しました。"
-  render 'index'
-end
-  
+  def update
+    if @message.update(message_params)
+      redirect_to root_path , notice: 'メッセージを編集しました'
+    else
+      render 'edit'
+    end
+  end
+    
+  def destroy
+    @message.destroy
+    redirect_to root_path, notice: 'メッセージを削除しました'
+  end
+
   private
+
   def message_params
     params.require(:message).permit(:name, :body)
+  end
+  
+  def set_message
+    @message = Message.find(params[:id])
   end
 end
