3fb956bdd38c8089207f26f1ab70c140ecb42d63
Youko Tada <counter0430culture@yahoo.co.jp>
2017-02-05 09:58:01 +0000

    Re-created message-board

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,5 +1,5 @@
 class Message < ActiveRecord::Base
     validates :name , length: { maximum: 20 } , presence: true
     validates :body , length: { minimum: 2 , maximum: 30 } , presence: true
-    validates :age , length: { minimum: 0 , maximum: 150 } , numericality: true
+    validates :age , numericality: { greater_than_or_equal_to: 0 , less_than_or_equal_to: 150 } , presence: true
 end
